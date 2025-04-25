5753337534a0560abc3dd3f85abf2fd5b2f66aaf
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2025-04-26 04:44:38 +0800

    feat: enable debug mode for development environment

--- src/woodcraft_db/woodcraft_db/settings.py
+++ src/woodcraft_db/woodcraft_db/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%5h6p#juk^l+!so4+ex96j9v3ju=cz+kn7^vap&73y=!r3^#wu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['woodcraft-backend.onrender.com', 'localhost']
 
